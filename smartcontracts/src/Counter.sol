d01fd24a9d87c823da0b84cbd80cc254cac73281
Guiventura6 <guilhermeventurasousa@gmail.com>
2024-03-18 22:09:48 -0300

    Ajustes no script de deploy

--- smartcontracts/src/Counter.sol
+++ smartcontracts/src/Counter.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.18;
 
 contract Counter {
     uint256 public number;
+    address owner;
+
+    error Unauthorized();
+
+    constructor() {
+        owner = 0x5112fDF253B858706e7103A419FE53AfD6a013D2;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -10,5 +17,20 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    function onlyOwner() internal view {
+        if (msg.sender != owner) {
+            revert Unauthorized();
+        }
+    }
+
+    function transferOwnership(address newOwner) external {
+        onlyOwner();
+        owner = newOwner;
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
     }
 }
