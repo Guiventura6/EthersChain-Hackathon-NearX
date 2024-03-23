137ab2d70589ff38d4034e818b550d2210d8df0b
Guiventura6 <guilhermeventurasousa@gmail.com>
2024-03-22 21:07:55 -0300

    Atualizando contrato

--- smartcontracts/src/AlbumNFT.sol
+++ smartcontracts/src/AlbumNFT.sol
@@ -9,13 +9,35 @@ contract AlbumNFT is ERC1155, ERC1155Burnable {
     uint public constant Figurinha_0 = 0;
     uint public constant Figurinha_1 = 1;
     uint public constant Figurinha_2 = 2;
+    uint public constant Figurinha_3 = 3;
+    uint public constant Figurinha_4 = 4;
+    uint public constant Figurinha_5 = 5;
+    uint public constant Figurinha_6 = 6;
+    uint public constant Figurinha_7 = 7;
+    uint public constant Figurinha_8 = 8;
+    uint public constant Figurinha_9 = 9;
+    uint public constant Figurinha_10 = 10;
+    uint public constant Figurinha_11 = 11;
 
-    uint[] public currentSupply = [50, 50, 50];
+    uint[] public currentSupply = [
+        50,
+        50,
+        50,
+        50,
+        50,
+        50,
+        50,
+        50,
+        50,
+        50,
+        50,
+        50
+    ];
 
     uint public tokenPrice = 0.01 ether;
 
     string public constant BASE_URL =
-        "ipfs://QmNW914SvRZ8sWRTPNLf98AX3xAyVToDmwJ8kz9XeZLWDD/";
+        "ipfs://QmWj6A5hMZ3tEo8RibfzcAVaDFxTYNur9itjrtt4jRF2xw/";
 
     address payable public immutable owner;
 
@@ -35,6 +57,11 @@ contract AlbumNFT is ERC1155, ERC1155Burnable {
     function uri(uint256 id) public pure override returns (string memory) {
         require(id < 3, "This token does not exists");
         return string.concat(BASE_URL, Strings.toString(id), ".json");
+    }
+
+    function transferNFT(address to, uint256 id) external {
+        require(balanceOf(msg.sender, id) > 0, "You do not own this NFT");
+        safeTransferFrom(msg.sender, to, id, 1, "");
     }
 
     function withdraw() external {
